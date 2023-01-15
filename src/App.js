7c8aeac7bbe3765f157be8e29e39dae89db5be8e
saqibashrafff <saqibashraf268@gmail.com>
2023-01-16 01:15:38 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [Fname, setFname] = useState("");
+  const [Lname, setLname] = useState("");
+
+  function FnameChange(event){
+
+    var Fnamevalue = event.target.value;
+
+    setFname(Fnamevalue);
+
+  }
+
+  function LnameChange(event){
+
+    var LnameValue = event.target.value;
+    
+    setLname(LnameValue);
+
+  }
+
+  function login(event){
+
+    if (Fname == "" || Lname == "" ){
+      alert("Please enter your credentials!");
+    }
+    else {
+      alert("Hello " + Fname + " " + Lname);
+
+      setLname("");
+      setFname("");
+      
+    }
+
+    event.preventDefault();
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='LoginSection' >
+      <h1>Greet You</h1><br></br>
+      <form onSubmit={login} >
+      <input value={Fname} type={"text"} placeholder="First Name" onChange={FnameChange}></input><br></br>
+      <input value={Lname} type={"text"} placeholder="Last Name" onChange={LnameChange} ></input><br></br>
+      <button type={"submit"} >Greet Me</button>
+      </form>
     </div>
   );
 }
